fe9be4e12542bdd8c93badbc6dee3aeab682c9e4
StarTitou <fatehdjehinet@gmail.com>
2020-04-11 07:02:07 +0200

    sec commit

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-app.get('/',(req,res)=>res.send('API Runnig'));
-const PORT = process.env.PORT || 5000;
 
+connectDB();
+
+app.use(express.json({extended:false}))
+
+
+app.get('/',(req,res)=>res.send('API Runnig'));
+
+
+app.use('/api/users',require('./routers/api/users'));
+app.use('/api/auth',require('./routers/api/auth'));
+app.use('/api/profile',require('./routers/api/profile'));
+app.use('/api/posts',require('./routers/api/posts'));
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server Started on Port '+PORT));
